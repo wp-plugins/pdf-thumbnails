dda22c068b86e1a330a4a6ca703e58bf357e80f5
stianlik <stianlik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-19 16:44:19 +0000

    Updated comment to reflect version 0.0.4
    git-svn-id: https://plugins.svn.wordpress.org/pdf-thumbnails/tags/0.0.4@1028774 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pdf-thumbnails.php
+++ pdf-thumbnails.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PDF Thumbnails
  * Description: Generates thumbnail for PDF-files when they are added.
- * Version: 0.0.3
+ * Version: 0.0.4
  * Author: Stian Liknes
  * License: WTFPL
  */
