08d5ae702b0bb5ecfb30a06e33aebbaeb5800ca7
stianlik <stianlik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-08 22:04:52 +0000

    Using dirname instead if __DIR__ for PHP 5.2 compability
    git-svn-id: https://plugins.svn.wordpress.org/pdf-thumbnails/tags/0.0.5@1040782 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pdf-thumbnails.php
+++ pdf-thumbnails.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: PDF Thumbnails
  * Description: Generates thumbnail for PDF-files when they are added.
- * Version: 0.0.3
+ * Version: 0.0.5
  * Author: Stian Liknes
  * License: WTFPL
  */
 
-require_once __DIR__ . '/PdfThumbnailsPlugin.php';
+require_once dirname(__FILE__). '/PdfThumbnailsPlugin.php';
 
 add_action('init', 'PdfThumbnailsPlugin::onInit');
