ac35383607df18652221fe3d3bc024e459fde49d
stianlik <stianlik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-28 19:23:03 +0000

    Delete PDF-thumbnails if PDF is deleted
    git-svn-id: https://plugins.svn.wordpress.org/pdf-thumbnails/trunk@1148354 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pdf-thumbnails.php
+++ pdf-thumbnails.php
@@ -9,4 +9,54 @@
 
 require_once dirname(__FILE__). '/PdfThumbnailsPlugin.php';
 
-add_action('init', 'PdfThumbnailsPlugin::onInit');
+add_action('admin_init', 'pdf_thumbnails_admin_int');
+
+function pdf_thumbnails_admin_int()
+{
+    if (!extension_loaded('imagick')) {
+        add_action('admin_notices', 'pdf_thumbnails_missing_imagick');
+        return;
+    }
+    add_filter('wp_generate_attachment_metadata', 'pdf_thumbnails_generate_attachment_metadata', 10, 2);
+    add_action('deleted_post', 'pdf_thumbnails_deleted_post');
+}
+
+   
+function pdf_thumbnails_generate_attachment_metadata($metadata, $attachment_id)
+{
+    if (get_post_mime_type($attachment_id) === 'application/pdf') {
+        PdfThumbnailsPlugin::instance()->regenerateThumbnail($attachment_id);
+    }
+    return $metadata;
+}
+
+function pdf_thumbnails_deleted_post($post_id)
+{
+    if (!$post_id) {
+        return;
+    }
+    
+    $attachments = get_posts(array(
+        'post_type' => 'attachment',
+        'post_status' => 'inherit',
+        'post_parent' => (int) $post_id,
+        'meta_key' => PdfThumbnailsPlugin::META_KEY
+    ));
+    
+    foreach ($attachments as $attachment) {
+        wp_delete_post($attachment->ID);
+    }
+}
+
+function pdf_thumbnails_missing_imagick()
+{
+    $message = sprintf(
+        __('The <a href="%s">ImageMagick</a> extension must be loaded to generate PDF thumbnails.'),
+        esc_url('http://php.net/manual/book.imagick.php')
+    );
+    ?>
+    <div class="error">
+        <p><?php echo $message; ?></p>
+    </div>
+    <?php
+}
